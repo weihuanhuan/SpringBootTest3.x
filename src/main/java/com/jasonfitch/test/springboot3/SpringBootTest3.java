8656afeb9fdca1a0b937214fb244374817bd021a
JasonFitch <weihuanhuan1107@126.com>
2024-05-23 20:09:38 +0800

    develop: init commit formatted spring boot 3.x project file.

---
+++ src/main/java/com/jasonfitch/test/springboot3/SpringBootTest3.java
@@ -0,0 +1,13 @@
+package com.jasonfitch.test.springboot3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTest3 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTest3.class, args);
+    }
+
+}
